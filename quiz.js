0c2ddeb1aa42ba1be3ce928dd9d1fb85b936faf5
Aryan <aryan.jain@stu.ocsb.ca>
2024-03-21 13:29:03 -0400

    grade calculator edit #1

--- quiz.js
+++ quiz.js
@@ -15,6 +15,19 @@ function fahrenheitToCelsius(fahrenheit) {
   
 //Program 2: Grade Calculator
 
+function calculateGrade(percentage) {
+    if (percentage >= 90) {
+        return 'A';
+    } else if (percentage >= 80 && percentage < 90) {
+        return 'B';
+    } else if (percentage >= 70 && percentage < 80) {
+        return 'C';
+    } else if (percentage >= 60 && percentage < 70) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
 //Program 3: Leap Year Checker
 
 //Program 4: Largest Number Finder
