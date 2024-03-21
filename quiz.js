931ad4ac9626e54cb6fe1e560a81531f4a07e027
Aryan <aryan.jain@stu.ocsb.ca>
2024-03-21 13:21:01 -0400

    convert temp code

--- quiz.js
+++ quiz.js
@@ -1,7 +1,18 @@
-const prompt = require('prompt-sync')();
+//const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
 
+function fahrenheitToCelsius(fahrenheit) {
+    // Convert Fahrenheit to Celsius with formula: (F - 32) * 5/9
+    var celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+  }
+  
+  // Example
+  var fahrenheitTemp = 67; // Example fahrenheit temp
+  var celsiusTemp = fahrenheitToCelsius(fahrenheitTemp);
+  console.log(fahrenheitTemp + " degrees Fahrenheit is equal to " + celsiusTemp + " degrees Celsius.");
+  
 //Program 2: Grade Calculator
 
 //Program 3: Leap Year Checker
